2bc69f8b5e7ec43e0c75b745afc503c7a3e084ed
Matthew Harrington <mattharrington576@gmail.com>
2016-10-05 16:16:38 -0400

    JAVASCRIPT WORKS PRAISE THE LORD

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,15 +1,53 @@
 var $ = require('jquery');
-var handlebars = require('handlebars');
+var Handlebars = require('handlebars');
 var _ = require('underscore');
 
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=deadpool+games&includes=Images,Shop&sort_on=score";
+
 fetchJSONP(url, function(data){
+
   var results = data.results;
   console.log(data);
+  displayProducts(results);
 });
 
+function displayProducts(products){
+  // template source
+  var source = $('#product-display-template').html();
+  // compiles a template so it can be executed immediately
+  var productTemplate = Handlebars.compile(source);
+  // each over every product in the products array
+  _.each(products, function(product){
+    // passes the product into the productTemplate to generated necessary HTML
+    var $productHtml = $(productTemplate(product));
+    // append the generated HTML to the .product-display element
+    $('.product-display').append($productHtml);
+  });
+
+};
 
 
 
+
+
+
+function displayCharacters(characterList){
+  console.log('displayCharacters');
+
+  var source = $('#character-template').html();
+  var characterTemplate = Handlebars.compile(source);
+
+  _.each(characterList, function(character){
+    var $characterHtml = $(characterTemplate(character));
+
+    $characterHtml.find('.js-character-button').on('click', function(event){
+      event.preventDefault();
+      fetchComics(character);
+    });
+
+    $('.characters').append($characterHtml);
+  });
+}
 
 
 
